9d5ee0a4db1ab3e9f0f5935caa2e0ff837a5429e
snapturtletom <tompham1705@gmail.com>
2026-02-19 11:58:52 +1100

    updated uvicorn port

--- app.py
+++ app.py
@@ -192,4 +192,4 @@ except Exception as e:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", "8000")))
