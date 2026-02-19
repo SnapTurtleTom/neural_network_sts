6ff49596b0ad74f37314223741868a5a18394c9a
snapturtletom <tompham1705@gmail.com>
2026-02-19 12:00:30 +1100

    idek

--- app.py
+++ app.py
@@ -192,4 +192,9 @@ except Exception as e:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", "8000")))
+    port_env = os.getenv("PORT", "8000")
+    try:
+        port = int(port_env)
+    except (TypeError, ValueError):
+        port = 8000
+    uvicorn.run(app, host="0.0.0.0", port=port)
