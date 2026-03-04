4442c5657d922078475732c4fdd5eae9a70c8c57
snapturtletom <tompham1705@gmail.com>
2026-03-04 13:52:49 +1100

    hopefully fixed port issue? again, fuck off uvicorn

--- app.py
+++ app.py
@@ -124,7 +124,10 @@ def upload_page_drawing():
 # Run Server
 # -----------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    try:
+        port = int(os.environ.get("PORT", 5000))
+    except (ValueError, TypeError):
+        port = 5000
 
     app.run(
         host="0.0.0.0",
